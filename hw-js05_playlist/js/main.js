2e27c5314fb074ae20c2eec501337d5bd06f2147
atolmachov <andreytolma4ov@gmail.com>
2018-12-12 00:04:36 +0200

    Fix hw-js05-playlist

--- hw-js05_playlist/js/main.js
+++ hw-js05_playlist/js/main.js
@@ -43,15 +43,17 @@
         ];
 
         var $playList = $('<div />', {id: 'play-list'}).appendTo('body');
-        $('<h1>PlayList</h1>').appendTo('#play-list')/*.append('<hr />')*/;
+        $('<h1>PlayList</h1>').appendTo('#play-list');
         for (var k = 0; k < playList.length; k++) {
             $('<div />')
                 .append('<hr />')
                 .append('<span class="duration">' + playList[k].duration +  '</span>')
                 .append('<span class="author">' + playList[k].author + '</span>')
                 .append('<h3>' + playList[k].song + '</h3>')
+                .append('<span class="like" />')
                 .appendTo($playList);
         }
+        $('.like').append('<svg />');
     });
 
 })(jQuery);
