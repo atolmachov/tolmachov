0b20ab4008f723b274a5eeb979f63d7bed001025
atolmachov <andreytolma4ov@gmail.com>
2018-12-19 00:39:04 +0200

    add exam-2

---
+++ exam-2-monticello/js/main.js
@@ -0,0 +1,34 @@
+;(function($){
+    $(function(){
+        $('.slick-slider').slick({
+            // dots: true,
+            infinite: true,
+            // autoplay: true,
+            autoplaySpeed: 1000,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });
+    });
+})(jQuery);
+var map,
+    infoWindowContent = document.getElementsByClassName('info-freeUA')[0].outerHTML;
+function initMap() {
+    var monticello = {lat: 40.679943, lng: -73.9309167},
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: monticello
+        }),
+        marker = new google.maps.Marker({
+            position: monticello,
+            map: map,
+            title: 'Monticello'
+        }),
+        infoWindow = new google.maps.InfoWindow ({
+            content: infoWindowContent
+        });
+    marker.setMap(map);
+    marker.addListener('click', function () {
+        infoWindow.open(map,  marker);
+    })
+}
+
