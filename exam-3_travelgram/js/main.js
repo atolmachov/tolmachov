2468bb255a764bc84b2c925233cce909409d1701
atolmachov <andreytolma4ov@gmail.com>
2019-02-09 23:23:32 +0200

    add exam-3 middle

--- exam-3_travelgram/js/main.js
+++ exam-3_travelgram/js/main.js
@@ -12,3 +12,24 @@
         });
     });
 })(jQuery);
+// var map,
+//     infoWindowContent = document.getElementsByClassName('info-freeUA')[0].outerHTML;
+function initMap() {
+    var navigator = {lat: 12.9768701, lng: 77.589954},
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: navigator
+        }),
+        marker = new google.maps.Marker({
+            position: navigator,
+            map: map,
+            title: 'You are this'
+        }),
+        infoWindow = new google.maps.InfoWindow ({
+            content: infoWindowContent
+        });
+    marker.setMap(map);
+    marker.addListener('click', function () {
+        infoWindow.open(map,  marker);
+    })
+}
