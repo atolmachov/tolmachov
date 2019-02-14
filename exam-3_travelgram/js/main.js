941d2bb3c2b98815720c9e42346bdb02f554b5d6
atolmachov <andreytolma4ov@gmail.com>
2019-02-15 01:06:39 +0200

    add exam-3 all-final

--- exam-3_travelgram/js/main.js
+++ exam-3_travelgram/js/main.js
@@ -10,17 +10,37 @@
             slidesToShow: 1,
             slidesToScroll: 1
         });
-        var menu = $('nav > ul');
+        var menu = $('nav');
+/*меню*/
         $('.hamburger').on('click', function () {
-            // menu.toggle();
             menu.toggleClass('visible');
             $(this).toggleClass('menu-opened');
         });
+/*добавление стрелки*/
+        $(window).scroll(function() {
+            var height = $(window).scrollTop();
+            if(height > 300){
+                $('.arrow').addClass('arrow-fixed');
+            } else{
+                $('.arrow').removeClass('arrow-fixed');
+            }
+        });
+/*анимация пролистывания страницы*/
+        $('a[href^="#"]').click(function (e) {
+            var href = $(this).attr('href'),
+                element = $(href);
+            e.preventDefault();
+            if(href == '#') return false;
+            if(element.length==0) return false;
+            var posTop = element.offset().top,
+                scrollTop = $(window).scrollTop;
+            $("html, body").animate({ scrollTop: posTop + "px" },1000);
+            console.log(posTop);
+        });
     });
 })(jQuery);
-
+/*Гугл-карта*/
 var marker;
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
@@ -35,7 +55,7 @@ function initMap() {
     });
     marker.addListener('click', toggleBounce);
 }
-
+/*анимация маркера*/
 function toggleBounce() {
     if (marker.getAnimation() !== null) {
         marker.setAnimation(null);
